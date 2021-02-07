47007a8932b8e5cd75510ec7ce8a211bbac5a7b5
Doaa663 <76392302+Doaa663@users.noreply.github.com>
2021-02-07 18:24:10 +0200

    edit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*global $ , window*/
+$(function (){
+   'use strict';
+    
+    $(window).scroll(function (){
+        var navbar = $('.navbar');
+        if ($(window).scrollTop() >= navbar.height()){
+            navbar.addClass("scrolled");   
+        } else {
+              navbar.removeClass("scrolled"); 
+        }
+        
+    });
+});
